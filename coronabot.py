3d13eb2a75022b8f8b12e1d8db099ff276964a25
Francesco <pham.francesco@gmail.com>
2020-03-29 20:26:37 +0200

    fix private message

--- coronabot.py
+++ coronabot.py
@@ -45,7 +45,7 @@ def on_message(bot, channel, sender, message):
                 response = corona.elaborate_query(query)
                 bot.send_message(channel, response)
     elif message.startswith("!news"):
-        query = query.split(" ", 1)
+        query = message.split(" ", 1)
         if len(query)>1:
             news_query = query[1]
             response = news.get_latest_news(news_query)
@@ -56,12 +56,16 @@ def on_message(bot, channel, sender, message):
     elif message == "!help":
         response = "!corona <location> for latest coronavirus report for specified location. \
                     !news <query> for latest news related to specified query."
+        bot.send_message(channel, response)
+
+def on_private_message(bot, sender, message):
+    on_message(bot, sender, sender, message)
 
 
 bot.on_connect.append(on_connect)
 bot.on_welcome.append(on_welcome)
 bot.on_public_message.append(on_message)
-bot.on_private_message.append(on_message)
+bot.on_private_message.append(on_private_message)
 
 bot.connect(irc_server_address)
 bot.run_loop()
