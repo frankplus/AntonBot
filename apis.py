436694974020abe5c07b03f6a576bd447034b374
Francesco <pham.francesco@gmail.com>
2020-04-20 22:55:34 +0200

    fix

--- apis.py
+++ apis.py
@@ -66,7 +66,11 @@ def get_youtube_videoinfo(item):
     description = item["snippet"]["description"]
     description = description[:150] if len(description) > 150 else description
     description = description.replace('\n', ' ')
-    publish_date = datetime.datetime.strptime(item["snippet"]["publishedAt"], "%Y-%m-%dT%H:%M:%S.%f%z")
+    publish_date = item["snippet"]["publishedAt"]
+    try:
+        publish_date = datetime.datetime.strptime(item["snippet"]["publishedAt"], "%Y-%m-%dT%H:%M:%S.%f%z")
+    except :
+        publish_date = datetime.datetime.strptime(item["snippet"]["publishedAt"], "%Y-%m-%dT%H:%M:%S%z")
     publish_date = publish_date.strftime("%b %d %Y")
     return "{} on {} \x02{} →\x02 {}".format(channel, publish_date, title, description)
 
